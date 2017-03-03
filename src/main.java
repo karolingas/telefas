f913de883bc120eb918c4c10b3d59d0c60d22913
Veronika <karolis.kojala@gmail.com>
2017-03-03 20:22:26 +0200

    sukėliau main klasę

--- src/main.java
+++ src/main.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 /**
  * Created by Veronika on 2017-03-03.
  */
 public class main {
     public static void main (String[] args){
-        System.out.print("Hello world!");
-        //сссс
+        double x,y;
+        System.out.println("Įveskite savo masę kg.");
+        Scanner in = new Scanner(System.in);
+        x = in.nextDouble();
+        System.out.println("Įveskite savo ūgį metrais");
+        y = in.nextDouble();
+        System.out.println("Jūsų KMI" +KMI(x,y));
+
+
+    } public static double KMI(double x, double y) {
+        double z, d;
+        z = x/(y*y);
+        d = Math.floor(z);
+        return (d);
     }
 }
