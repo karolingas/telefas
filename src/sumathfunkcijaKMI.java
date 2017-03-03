16dfb8a79287983e28528319fb1d141d2a44c74a
Veronika <karolis.kojala@gmail.com>
2017-03-03 20:41:40 +0200

    sukėliau main klasę

---
+++ src/sumathfunkcijaKMI.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+/**
+ * Created by Veronika on 2017-03-03.
+ */
+public class sumathfunkcijaKMI {
+    public static void main (String[] args){
+        double x,y;
+        System.out.println("Įveskite savo masę kg.");
+        Scanner in = new Scanner(System.in);
+        x = in.nextDouble();
+        System.out.println("Įveskite savo ūgį metrais");
+        y = in.nextDouble();
+        y = Math.pow(y,2);
+        System.out.println("Jūsų KMI" + KMI(x,y));
+
+
+    } public static double KMI(double x, double y) {
+        double z, d;
+
+        z = x/y;
+        d = Math.floor(z);
+        return (d);
+    }
+}
