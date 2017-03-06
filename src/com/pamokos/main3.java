90a7854bcf0c0062e9e326466d99e7dd94c91290
Veronika <karolis.kojala@gmail.com>
2017-03-06 19:58:22 +0200

    sukėliau main klasę

--- src/com/pamokos/main3.java
+++ src/com/pamokos/main3.java
@@ -6,15 +6,25 @@ import com.užduotis3.Užduotis3;
 import com.užduotis4.Užduotis4;
 import com.užduotis5.Užduotis5;
 
+import java.util.Scanner;
 
 
 public class main3 {
     public static void main(String[] args) {
         Užduotis1 uzduotis1 = new Užduotis1();
-        if ()
-        Užduotis2 uzduotis2 = new Užduotis2();
-        Užduotis3 uzduotis3 = new Užduotis3();
+        System.out.println("pasirinkite nuo 1 iki 4");
+        Scanner sc = new Scanner(System.in);
+        Integer x = sc.nextInt();
+        if (x == 1){
+            Užduotis2 uzduotis2 = new Užduotis2();
+        } else if (x == 2) {
+            Užduotis3 uzduotis3 = new Užduotis3();
+        } else if (x == 3) {
         Užduotis4 uzduotis4 = new Užduotis4();
-        Užduotis5 uzduotis5 = new Užduotis5();
+        } else if (x == 4) {
+            Užduotis5 uzduotis5 = new Užduotis5();
+        } else {
+            System.out.println("Tokio pasirinkimo nėra rinkis nuo 1 iki 4");
+        }
     }
 }
--- src/com/užduotis5/Užduotis5.java
+++ src/com/užduotis5/Užduotis5.java
@@ -12,10 +12,9 @@ public class Užduotis5 {
         int x = in.nextInt();
         System.out.println("Įveskite kiek litrų sunaudojo");
         double y = in.nextDouble();
-        y = Math.pow(y, 2);
-        double z = (x*100)/y;
+        double z = (y/x)*100;
         double d = Math.floor(z);
 
-        System.out.println("Jūsų automobilis vidutinškai suvartoja" + d + "l/100km.");
+        System.out.println("Jūsų automobilis vidutinškai suvartoja " + d + " l/100km.");
     }
 }
